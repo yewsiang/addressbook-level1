34493c82008247f7ef9244870d7808aa3b72dbfd
yewsiang <tangys108@gmail.com>
2016-08-24 12:33:17 +0800

    Broke SLAP in the main argument (T2A2)

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -199,9 +199,25 @@ public class AddressBook {
      * ====================================================================
      */
     public static void main(String[] args) {
-        showWelcomeMessage();
-        processProgramArgs(args);
-        loadDataFromStorage();
+        //showWelcomeMessage();
+    	showToUser(DIVIDER, DIVIDER, VERSION, MESSAGE_WELCOME, DIVIDER);
+        
+        // processProgramArgs(args);
+        // Tutorial 2 [LO-MethodAbstraction] 
+        if (args.length >= 2) {
+            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+            exitProgram();
+        }
+        if (args.length == 1) {
+            setupGivenFileForStorage(args[0]);
+        }
+        if(args.length == 0) {
+            setupDefaultFileForStorage();
+        }
+        
+        // loadDataFromStorage();
+        initialiseAddressBookModel(loadPersonsFromFile(storageFilePath));
+        
         while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
